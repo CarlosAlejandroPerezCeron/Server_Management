67664f1182078f9f336595c554ff93d73980c13f
Carlos Alejandro Perez Ceron <89228822+CarlosAlejandroPerezCeron@users.noreply.github.com>
2024-12-04 22:12:52 -0500

    main.tf

---
+++ ansible/vars ansible/roles/nano terraform/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "WebServer"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo amazon-linux-extras install nginx1 -y",
+      "sudo systemctl start nginx",
+      "sudo systemctl enable nginx"
+    ]
+  }
+}
